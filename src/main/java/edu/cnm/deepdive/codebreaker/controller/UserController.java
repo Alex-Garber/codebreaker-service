0e277d9aa557fa41bc842e166244cceb21e4a386
Alex Garber <alexgarber6@gmail.com>
2020-11-06 10:01:48 -0700

    Started HATEOAS implemtentation

--- src/main/java/edu/cnm/deepdive/codebreaker/controller/UserController.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/controller/UserController.java
@@ -2,26 +2,39 @@ package edu.cnm.deepdive.codebreaker.controller;
 
 import edu.cnm.deepdive.codebreaker.model.entity.entity.User;
 import edu.cnm.deepdive.codebreaker.service.UserService;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.ExposesResourceFor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@ExposesResourceFor(User.class)
 public class UserController {
 
   private final UserService userService;
 
-@Autowired
+  @Autowired
   public UserController(UserService userService) {
     this.userService = userService;
   }
+
   @GetMapping(value = "/me", produces = MediaType.APPLICATION_JSON_VALUE)
   public User me(Authentication auth) {
-  return (User) auth.getPrincipal();
+    return (User) auth.getPrincipal();
+  }
+
+  @GetMapping(value = "/{userId:[0-9a-fA-F\\-]{32,}}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public User get(@PathVariable UUID userId, Authentication auth) {
+    return userService.getUser(userId)
+        .orElseThrow(NoSuchElementException::new);
   }
 
 }
+
